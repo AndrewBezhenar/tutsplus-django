a65be9747c8d725e79a4e6792b8e50388c1f426a
Andrew Bezhenar <andbezh@gmail.com>
2015-01-25 10:18:12 +0200

    vote fix needed

--- stories/models.py
+++ stories/models.py
@@ -1,13 +1,14 @@
-from django.db import models
-
 from urlparse import urlparse
+
+from django.db import models
 from django.contrib.auth.models import User
 
 class Story(models.Model):
 	title = models.CharField(max_length=200)
 	url = models.URLField()
 	points = models.IntegerField(default=1)
-	moderator = models.ForeignKey(User)
+	moderator = models.ForeignKey(User, related_name='moderated_stories')
+	voters = models.ManyToManyField(User, related_name='liked_stories')
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
